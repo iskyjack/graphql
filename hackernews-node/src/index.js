16463a65fce42bab2ec511a01749e84471cad57c
iskyjack <39756644+iskyjack@users.noreply.github.com>
2019-04-06 19:35:36 +0530

    Add files via upload

---
+++ hackernews-node/src/index.js
@@ -0,0 +1,42 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+// 1
+const typeDefs = `
+type Query {
+  info: String!
+}
+`
+
+// 2
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+  // 1
+  let idCount = links.length
+  const resolvers = {
+    Query: {
+      info: () => `This is the API of a Hackernews Clone`,
+      feed: () => links,
+    },
+    Mutation: {
+      // 2
+      post: (root, args) => {
+         const link = {
+          id: `link-${idCount++}`,
+          description: args.description,
+          url: args.url,
+        }
+        links.push(link)
+        return link
+      }
+    },
+  }
+
+// 3
+const server = new GraphQLServer({
+    typeDefs: './schema.graphql',
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
